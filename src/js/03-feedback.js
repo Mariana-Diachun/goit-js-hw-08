31420a071374bb16873decad7f0c8ff8c5a942ae
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-20 17:44:42 +0200

    added changes to task 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackformData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+    input: document.querySelector('input'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+
+populateTextarea();
+
+function onFormInput(evt) {
+    feedbackformData[evt.target.name] = evt.target.value;
+    const storageData = JSON.stringify(feedbackformData);
+    localStorage.setItem(STORAGE_KEY, storageData)
+}
+
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function populateTextarea () {
+    const savedStoragedata = localStorage.getItem(STORAGE_KEY)
+        if (savedStoragedata) {
+            console.log(JSON.parse(savedStoragedata));
+    }
+}
 
