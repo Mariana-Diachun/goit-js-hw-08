0b50d9797e263d7962bfea5f7615bb94d721c9cc
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-20 19:00:54 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,34 @@
 import Player from '@vimeo/player';
 console.log(Player);
 
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframe);
+    console.log(iframePlayer);
+    
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+iframePlayer.on('play', onTimeUpdate)
+    
+function onTimeUpdate() {
+    seconds: 3.034
+}
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-    player.setCurrentTime(30.456).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+
+
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+//     player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
